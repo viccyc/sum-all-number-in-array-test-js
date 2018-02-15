1ede0b49a01f04b4a892c67b8fd250c363a3d403
Viccy Grace <viccy.c.grace@gmail.com>
2018-02-15 16:46:08 +0000

    recursive array function complete

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+let sum = 0;
 function sumItems(array) {
   // Sum all the numbers in the array
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      sumItems(item);
+    } else {
+      sum += item;
+    }
+  });
+  return sum;
 }
 
 module.exports = sumItems;
+
+// console.log(sumItems([1, [2, [3]], 4, 5]));
+// console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+// console.log(sumItems([[[[[[[[[[[[[[1]]]]]]]]]]]]]]));
+
